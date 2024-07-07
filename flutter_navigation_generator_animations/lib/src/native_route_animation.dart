ba02c3dc56bfca4f9fac0b5ba3011953b36748b3
WilliamVerhaeghe <william.verhaeghe@icapps.com>
2024-07-07 11:11:50 +0200

    Fixed issue with flutter 3.22

--- flutter_navigation_generator_animations/lib/src/native_route_animation.dart
+++ flutter_navigation_generator_animations/lib/src/native_route_animation.dart
@@ -2,21 +2,24 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_navigation_generator_animations/src/zoom_page_transition.dart';
 
 class NativeRouteAnimation<T> extends PageRouteBuilder<T> {
   final Function(BuildContext context) builder;
   final bool iOSLinearTransition;
+  final bool androidAllowSnapshotting;
+  final bool androidAllowEnterRouteSnapshotting;
 
   NativeRouteAnimation({
     required this.builder,
     this.iOSLinearTransition = true,
+    this.androidAllowSnapshotting = true,
+    this.androidAllowEnterRouteSnapshotting = true,
     super.settings,
     super.fullscreenDialog,
     super.transitionDuration,
   }) : super(
-          pageBuilder: (context, animation, secondaryAnimation) =>
-              builder(context),
+          pageBuilder: (context, animation, secondaryAnimation) => builder(context),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             if (kIsWeb) return child;
             if (Platform.isIOS || Platform.isMacOS) {
@@ -28,12 +31,12 @@ class NativeRouteAnimation<T> extends PageRouteBuilder<T> {
               );
             }
             if (Platform.isAndroid) {
-              return ZoomPageTransitionsBuilder().buildTransitions(
-                null,
-                context,
-                animation,
-                secondaryAnimation,
-                child,
+              return ZoomPageTransition(
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                allowSnapshotting: androidAllowSnapshotting,
+                allowEnterRouteSnapshotting: androidAllowEnterRouteSnapshotting,
+                child: child,
               );
             }
             return FadeTransition(
